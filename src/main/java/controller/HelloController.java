f380f0cc82914afb5ed0e123459455d50f4356bd
Martin Wang <martinwangjun@163.com>
2017-11-22 14:22:45 +0800

    Controller Sample program

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -5,7 +5,9 @@ import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -13,12 +15,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController{
     private static final Log logger = LogFactory.getLog(HelloController.class);
     
-    @RequestMapping(value="/hello2")
+    @RequestMapping(value="/hello")
     public ModelAndView hello() {
         logger.info("hello方法被调用");
         ModelAndView mv = new ModelAndView();
         mv.addObject("message", "Hello World by Annotation! @ " + new Date());
         mv.setViewName("/WEB-INF/content/welcome.jsp");
         return mv;
+    }
+    
+    @RequestMapping(value="/helloWorld", method=RequestMethod.GET)
+    public String helloWorld(Model model) {
+        return "helloWorld";
     }
 }
---
+++ src/main/webapp/WEB-INF/content/helloWorld.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>JSP HTML5</title>
+<link href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+<script src="http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<h3>Hello World!</h3>
+<p>模板内的表达式非常便利，但是设计它们的初衷是用于简单运算的。在模板中放入太多的逻辑会让模板过重且难以维护。</p>
+</div>
+</body>
+</html>
