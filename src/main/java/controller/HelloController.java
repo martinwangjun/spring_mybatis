b5a12f1e1f62ad0350e3046d9fc00ee5ab88ee47
Martin Wang <martinwangjun@163.com>
2017-11-27 11:19:44 +0800

    change logger

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.util.Date;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
